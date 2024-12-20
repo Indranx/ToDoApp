0a9545b656559dbd575f753295c9ce1b26ef3b0e
Indran <indranstv@gmail.com>
2024-12-20 23:37:28 +0800

    Version 2

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-
 import Header from "./Header";
-import Notes from "./Notes";
 import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createCard(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Notes />
+      {notes.map(createCard)}
       <Footer />
     </div>
   );
